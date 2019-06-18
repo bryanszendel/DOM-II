b537a86b9c110d14aaf797e71fcc0eba2aecd794
Bryan Szendel <bryan.szendel@gmail.com>
2019-06-18 13:59:22 -0600

    keydown event listener added

--- js/index.js
+++ js/index.js
@@ -1,10 +1,19 @@
 // MOUSEOVER
-let test = document.querySelector('nav');
-test.addEventListener('mouseover', function(event) {
+let mousingOver = document.querySelector('nav');
+mousingOver.addEventListener('mouseover', function(event) {
     event.target.style.color = 'dodgerblue';
 
     setTimeout(function() {
         event.target.style.color = "";
-    }, 1000);
+    }, 500);
     }, false);
 
+// KEYDOWN
+let keyDown = document.querySelector('body');
+keyDown.addEventListener('keydown', function(event) {
+    if (event) {
+        alert('Why are you typing here?');
+    }
+})
+
+// 
