b43142b8796f8e49fbd856c0ede69313e5e2e976
Bryan Szendel <bryan.szendel@gmail.com>
2019-06-18 13:38:29 -0600

    mouseover event listener added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-// Your code goes here
+// MOUSEOVER
+let test = document.querySelector('nav');
+test.addEventListener('mouseover', function(event) {
+    event.target.style.color = 'dodgerblue';
+
+    setTimeout(function() {
+        event.target.style.color = "";
+    }, 1000);
+    }, false);
+
